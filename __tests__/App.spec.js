2ad67303f57771696db4fc260edbe5ce98eda1dc
Rafael Ruiz <rmoreno@competa.com>
2018-03-06 12:38:41 +0100

    add babel conf and init tests

---
+++ __tests__/App.spec.js
@@ -0,0 +1,26 @@
+import { shallow, createLocalVue } from '@vue/test-utils'
+import App from '../src/App'
+import Vuex from 'vuex'
+import { store } from '../src/store'
+
+const localVue = createLocalVue()
+
+localVue.use(Vuex)
+
+describe('App', () => {
+  let app
+  let store
+
+  beforeEach(() => {
+    app = { }
+    store = new Vuex.Store({
+      state: {},
+      app
+    })
+  })
+
+  it ('is a Vue instance', () => {
+    const wrapper = shallow(App, { store, localVue })
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+})
