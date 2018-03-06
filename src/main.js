bdd5f237144b499b44d0daeb2285bc4173cff3bc
Rafael Ruiz <rmoreno@competa.com>
2018-03-06 13:35:31 +0100

    init

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
 
 Vue.config.productionTip = false
-
-import { store } from './store';
 
 new Vue({
   store,
--- src/store.js
+++ src/store.js
@@ -3,21 +3,14 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export const store = new Vuex.Store({
+export default new Vuex.Store({
   state: {
-    selectedUser: '',
-    searchQuery: ''
+
   },
   mutations: {
-    clearSelectedUser (state) {
-      state.selectedUser = ''
-    },
-    setSelectedUser (state, newSelectedUser) {
-      state.selectedUser = newSelectedUser
-    },
-    setSearchQuery (state, newSearchQuery) {
-      state.searchQuery = newSearchQuery
-    }
+
+  },
+  actions: {
+
   }
 })
-
